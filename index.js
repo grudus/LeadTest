0bcb8c07a92a3e290563180e7f0e7d5b6d5258f2
grudus <jakgruda@gmail.com>
2018-03-23 17:06:55 +0100

    Add multiple environments

---
+++ index.js
@@ -0,0 +1,50 @@
+const LOCAL_STORAGE_APP_ID = 'appId';
+const LOCAL_STORAGE_ENV = 'environment';
+
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const radios = document.querySelectorAll("input[type='radio']");
+
+let appId = localStorage.getItem(LOCAL_STORAGE_APP_ID);
+let environment = localStorage.getItem(LOCAL_STORAGE_ENV) || 'staging';
+
+radios.forEach(radio => {
+    radio.onclick = () => onRadioClick(radio);
+    if (radio.value == environment)
+        radio.checked = 'checked';
+});
+
+const onRadioClick = (radio) => {
+    localStorage.setItem(LOCAL_STORAGE_ENV, radio.value);
+    location.reload();
+};
+
+input.value = appId;
+
+button.addEventListener("click", () => {
+    localStorage.setItem(LOCAL_STORAGE_APP_ID, input.value);
+    location.reload();
+});
+
+window.CrazyCallSettings = {
+    appId,
+};
+
+
+const loadScript = (src) => {
+    var head = document.getElementsByTagName('head')[0],
+        script = document.createElement('script');
+    done = false;
+    script.setAttribute('src', src);
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('charset', 'utf-8');
+    script.onload = script.onreadstatechange = () => {
+        if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+            done = true;
+            script.onload = script.onreadystatechange = null;
+        }
+    }
+    head.insertBefore(script, head.firstChild);
+}
+
+loadScript(`https://crazy-website-widget.crazycall.com/${environment}/widget.js`)
